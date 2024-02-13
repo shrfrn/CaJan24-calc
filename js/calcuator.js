091f2aa4bcf9ab66cab9629f619852b817ecb2e3
shrfrn <shrfrn@gmail.com>
2024-02-13 10:52:06 +0200

    Add click handler placeholders

---
+++ js/calcuator.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function onNumClick(elCell) {
+    console.log('Number clicked', elCell.innerText)
+}
+
+function onOperatorClick(elCell) {
+    console.log('Operator clicked')
+}
+
+function onCalc() {
+    console.log('Calculating...')
+}
